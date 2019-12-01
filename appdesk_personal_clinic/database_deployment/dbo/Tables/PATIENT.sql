5b322fc45a4e4e98cde7e36af31495c0d7bcea3f
unknown <hyhoa2000@gmail.com>
2019-12-01 21:31:10 +0700

    create database project deployment sample data

---
+++ appdesk_personal_clinic/database_deployment/dbo/Tables/PATIENT.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PATIENT]
+(
+	[idPatient] INT NOT NULL PRIMARY KEY IDENTITY(100, 1), 
+    [idPassport] NVARCHAR(20) NOT NULL, 
+    [fullName] NVARCHAR(50) NULL, 
+    [birthday] NVARCHAR(20) NULL, 
+    [healthStatus] INT NULL
+)
---
+++ appdesk_personal_clinic/database_deployment/post_pulish_scripts/Script.Deployments.sql
@@ -0,0 +1,25 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO dbo.SPECIALIST (fullName, location)
+VALUES
+('Medical_01','A01'),
+('Medical_02','A02'),
+('Medical_03','A03');
+
+INSERT INTO dbo.DOCTOR (idSpecialist, idPassport, fullName, isGeneral)
+VALUES
+(10, '16520001', 'Alex', 1),
+(10, '16520002', 'Meri', 1),
+(11, '16520003', 'Jack', 0),
+(12, '16520004', 'Kari', 0),
+(12, '16520005', 'Erik', 0);
