dac89837f3899bbf41d882129a75e2d304786ae2
unknown <hyhoa2000@gmail.com>
2019-12-02 15:52:18 +0700

    creat crud - update in data doctortable and custom ui this update

--- appdesk_personal_clinic/business_logic_layer/DoctorBUS.cs
+++ appdesk_personal_clinic/business_logic_layer/DoctorBUS.cs
@@ -14,10 +14,31 @@ namespace business_logic_layer
     {
         public DoctorDAL doctorDal = new DoctorDAL();
 
+        #region Logical CRUD Methods
+
         public PersonalClinicDataSet.DOCTORDataTable getData()
         {
             PersonalClinicDataSet.DOCTORDataTable _tblDoctor = doctorDal.getData();
             return _tblDoctor;
         }
+
+        public int updateDoctor(
+            string idSpecialist,
+            string idPassport,
+            string fullName,
+            string isGeneral,
+            string originalIdDoctor) {
+
+            DoctorDTO currentDoctorDatarow = new DoctorDTO();
+            currentDoctorDatarow.IdSpecialist = Convert.ToInt32(idSpecialist);
+            currentDoctorDatarow.IdPassport = Convert.ToString(idPassport);
+            currentDoctorDatarow.FullName = Convert.ToString(fullName);
+            currentDoctorDatarow.IsGeneral = Convert.ToInt32(isGeneral);
+            currentDoctorDatarow.IdDoctor = Convert.ToInt32(originalIdDoctor);
+
+            return doctorDal.updateCoreDoctor(currentDoctorDatarow);
+        }
+
+        #endregion Logical CRUD Methods
     }
 }
--- appdesk_personal_clinic/presentation_layer/search_screen.cs
+++ appdesk_personal_clinic/presentation_layer/search_screen.cs
@@ -10,12 +10,14 @@ using System.Windows.Forms;
 
 using business_logic_layer;
 using database_access_layer;
+using data_transfer_object;
 
 namespace presentation_layer
 {
     public partial class search_screen : Form
     {
-        public DoctorBUS _doctorBus = new DoctorBUS();
+        public DoctorBUS doctorBus = new DoctorBUS();
+        public DoctorDTO doctorState = new DoctorDTO();
 
         public search_screen()
         {
@@ -24,20 +26,72 @@ namespace presentation_layer
 
         #region Methods
 
-        public void loadList()
-        {
-            PersonalClinicDataSet.DOCTORDataTable _tableDoctor = _doctorBus.getData();
+        #region CRUD
 
-            listBox1.DisplayMember = "fullName";
-            listBox1.ValueMember = "idDoctor";
-            listBox1.DataSource = _tableDoctor;
+        public void getDataGrid()
+        {
+            PersonalClinicDataSet.DOCTORDataTable tableDoctor = doctorBus.getData();
+            dgvDoctor.DataSource = tableDoctor;
         }
+
+        public void updateDataPointDoctor(
+            string idSpecialistUI,
+            string idPassportUI,
+            string fullNameUI,
+            string isGeneralUI,
+            string originalIdDoctorUI)
+        {
+            doctorBus.updateDoctor(
+                idSpecialistUI, 
+                idPassportUI, 
+                fullNameUI, 
+                isGeneralUI, 
+                originalIdDoctorUI
+                );
+        }
+
+        public bool fillCurrentRowDataDoctorToControl()
+        {
+            tbxPassportDoctor.Text = dgvDoctor.CurrentRow.Cells["idPassport"].Value.ToString();
+            tbxFullnameDoctor.Text = dgvDoctor.CurrentRow.Cells["fullName"].Value.ToString();
+            cbxSpecialist.Text = dgvDoctor.CurrentRow.Cells["idSpecialist"].Value.ToString();
+            lbidDoctor.Text = dgvDoctor.CurrentRow.Cells["idDoctor"].Value.ToString();
+            lbisGeneral.Text = dgvDoctor.CurrentRow.Cells["isGeneral"].Value.ToString();
+
+            return true;
+        }
+
+        #endregion CRUD
 
         #endregion Methods
 
+        #region Events
+
         private void search_screen_Load(object sender, EventArgs e)
         {
-            loadList();
+            getDataGrid();
         }
+
+        private void dgvDoctor_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(dgvDoctor.RowCount > 0)
+                fillCurrentRowDataDoctorToControl();
+            return;
+        }
+
+        private void btnUpdateDoctor_Click(object sender, EventArgs e)
+        {
+            updateDataPointDoctor(
+                cbxSpecialist.Text,
+                tbxPassportDoctor.Text,
+                tbxFullnameDoctor.Text,
+                lbisGeneral.Text,
+                lbidDoctor.Text
+                );
+
+            getDataGrid();
+        }
+
+        #endregion Events
     }
 }
