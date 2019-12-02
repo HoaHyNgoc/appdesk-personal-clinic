4566b0b39b8475112f8af06edc50e7f4c7bea045
unknown <hyhoa2000@gmail.com>
2019-12-02 23:03:46 +0700

    insert datapoints example to table

--- appdesk_personal_clinic/database_deployment/post_pulish_scripts/Script.Deployments.sql
+++ appdesk_personal_clinic/database_deployment/post_pulish_scripts/Script.Deployments.sql
@@ -10,16 +10,60 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
---INSERT INTO dbo.SPECIALIST (fullName, location)
+--INSERT INTO [dbo].[SPECIALIST] ([fullName], [location])
 --VALUES
---('Medical_01','A01'),
---('Medical_02','A02'),
---('Medical_03','A03');
+--('H(a)ematology','A01'),
+--('Immunology','A02'),
+--('Internal medicine','B01'),
+--('Traumatology','B02'),
+--('Surgery','C01');
+
+--INSERT INTO [dbo].[UNIT_MEDICINE] ([fullNameUnit]) 
+--VALUES 
+--('unit_01'),
+--('unit_02'),
+--('unit_03'),
+--('unit_04'),
+--('unit_05');
+
+--INSERT INTO [dbo].[PATIENT] ([idPassport], [fullName], [birthday], [healthStatus]) 
+--VALUES 
+--('17520000', 'Patient_01', '1998', 1),
+--('17520001', 'Patient_02', '1999', 1),
+--('17520002', 'Patient_03', '2000', 1),
+--('17520003', 'Patient_04', '1998', 1),
+--('17520004', 'Patient_05', '2000', 1);
+
+--INSERT INTO [dbo].[PHARMACIST] ([idPassport], [fullName]) 
+--VALUES 
+--('18520000', 'Pharmacist_01'),
+--('18520001', 'Pharmacist_02'),
+--('18520002', 'Pharmacist_03'),
+--('18520003', 'Pharmacist_04'),
+--('18520004', 'Pharmacist_05');
+
+/*-----------------------------------------------------------*/
 
 --INSERT INTO dbo.DOCTOR (idSpecialist, idPassport, fullName, isGeneral)
 --VALUES
---(10, '16520001', 'Alex', 1),
---(10, '16520002', 'Meri', 1),
---(11, '16520003', 'Jack', 0),
---(12, '16520004', 'Kari', 0),
---(12, '16520005', 'Erik', 0);
+--(2016, '16520000', 'Alex', 1),
+--(2016, '16520001', 'Meri', 1),
+--(2018, '16520002', 'Jack', 0),
+--(2018, '16520003', 'Kari', 0),
+--(2019, '16520004', 'Erik', 0);
+
+--INSERT INTO [dbo].[DIAGNOSTIC] ([idSpecialist], [numberRoom], [fullName]) 
+--VALUES 
+--(2016, 'P100', 'Diagnostic_01'),
+--(2016, 'P101', 'Diagnostic_02'),
+--(2018, 'P102', 'Diagnostic_03'),
+--(2018, 'P103', 'Diagnostic_04'),
+--(2016, 'P104', 'Diagnostic_05');
+
+--INSERT INTO [dbo].[MEDICINE] ([idUnitMedicine], [fullName], [symbol], [ingredient], [priceUnit]) 
+--VALUES 
+--(8000, 'Medicine_01', 'M01', 'ingredient_01', 10),
+--(8000, 'Medicine_02', 'M02', 'ingredient_02', 20),
+--(8001, 'Medicine_03', 'M03', 'ingredient_03', 30),
+--(8002, 'Medicine_01', 'M04', 'ingredient_04', 40),
+--(8002, 'Medicine_01', 'M05', 'ingredient_05', 10);
