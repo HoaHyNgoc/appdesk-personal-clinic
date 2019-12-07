07329b4806957d1b0cfb2ad228235c28672cb674
unknown <hyhoa2000@gmail.com>
2019-12-07 23:29:49 +0700

    will complete layer data transfer object

---
+++ appdesk_personal_clinic/data_transfer_object/BillDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_transfer_object
+{
+    public class BillDTO
+    {
+        public int idBill;
+        public int idPrescription;
+        public int idTechnique;
+        public int idPharmacist;
+        public decimal valuePayBill;
+        public DateTime dateInit;
+
+        public int IdBill { get => idBill; set => idBill = value; }
+        public int IdPrescription { get => idPrescription; set => idPrescription = value; }
+        public int IdTechnique { get => idTechnique; set => idTechnique = value; }
+        public int IdPharmacist { get => idPharmacist; set => idPharmacist = value; }
+        public decimal ValuePayBill { get => valuePayBill; set => valuePayBill = value; }
+        public DateTime DateInit { get => dateInit; set => dateInit = value; }
+    }
+}
---
+++ appdesk_personal_clinic/data_transfer_object/DetailPrescriptionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_transfer_object
+{
+    public class DetailPrescriptionDTO
+    {
+        public int idDetailPrescription;
+        public int idPrescription;
+        public int idMedicine;
+        public int number;
+        public string useHelp;
+        public decimal price;
+
+        public int IdDetailPrescription { get => idDetailPrescription; set => idDetailPrescription = value; }
+        public int IdPrescription { get => idPrescription; set => idPrescription = value; }
+        public int IdMedicine { get => idMedicine; set => idMedicine = value; }
+        public int Number { get => number; set => number = value; }
+        public string UseHelp { get => useHelp; set => useHelp = value; }
+        public decimal Price { get => price; set => price = value; }
+    }
+}
