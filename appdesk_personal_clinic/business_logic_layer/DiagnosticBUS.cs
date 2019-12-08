fabf0466bb28c5c35e6f4c10f52e6664bda4d7f1
unknown <hyhoa2000@gmail.com>
2019-12-08 15:02:34 +0700

    will complete business layer

---
+++ appdesk_personal_clinic/business_logic_layer/DiagnosticBUS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using database_access_layer;
+using data_transfer_object;
+
+namespace business_logic_layer
+{
+    public class DiagnosticBUS
+    {
+        public DiagnosticDAL diagnosticDal = new DiagnosticDAL();
+
+        #region Logical CRUD Methods
+
+        public PersonalClinicDataSet.DIAGNOSTICDataTable getData()
+        {
+            PersonalClinicDataSet.DIAGNOSTICDataTable _tblDiagnostic = diagnosticDal.getData();
+            return _tblDiagnostic;
+        }
+
+        public int updateDiagnostic(
+            string idSpecialist,
+            string numberRoom,
+            string fullName,
+            string originalIdDiagnostic)
+        {
+
+            DiagnosticDTO currentDiagnosticDatarow = new DiagnosticDTO();
+            currentDiagnosticDatarow.IdSpecialist = Convert.ToInt32(idSpecialist);
+            currentDiagnosticDatarow.NumberRoom = Convert.ToString(numberRoom);
+            currentDiagnosticDatarow.FullName = Convert.ToString(fullName);
+            currentDiagnosticDatarow.IdDiagnostic = Convert.ToInt32(originalIdDiagnostic);
+
+            return diagnosticDal.updateCoreDiagnostic(currentDiagnosticDatarow);
+        }
+
+        public int insertDiagnostic(
+            string idSpecialist,
+            string numberRoom,
+            string fullName)
+        {
+
+            DiagnosticDTO currentDiagnosticDatarow = new DiagnosticDTO();
+            currentDiagnosticDatarow.IdSpecialist = Convert.ToInt32(idSpecialist);
+            currentDiagnosticDatarow.NumberRoom = Convert.ToString(numberRoom);
+            currentDiagnosticDatarow.FullName = Convert.ToString(fullName);
+
+            return diagnosticDal.insertCoreDiagnostic(currentDiagnosticDatarow);
+        }
+
+        #endregion Logical CRUD Methods
+    }
+}
---
+++ appdesk_personal_clinic/business_logic_layer/PrescriptionBUS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using database_access_layer;
+using data_transfer_object;
+
+namespace business_logic_layer
+{
+    public class PrescriptionBUS
+    {
+        public PrescriptionDAL prescriptionDal = new PrescriptionDAL();
+
+        #region Logical CRUD Methods
+
+        public PersonalClinicDataSet.PRESCRIPTIONDataTable getData()
+        {
+            PersonalClinicDataSet.PRESCRIPTIONDataTable _tblPrescription = prescriptionDal.getData();
+            return _tblPrescription;
+        }
+
+        public int updatePrescription(
+            string idPatient,
+            string conclusionMedical,
+            string price,
+            string originalIdPrescription)
+        {
+
+            PrescriptionDTO currentPrescriptionDatarow = new PrescriptionDTO();
+            currentPrescriptionDatarow.IdPatient = Convert.ToInt32(idPatient);
+            currentPrescriptionDatarow.ConclusionMedical = Convert.ToString(conclusionMedical);
+            currentPrescriptionDatarow.Price = Convert.ToDecimal(price);
+            currentPrescriptionDatarow.IdPrescription = Convert.ToInt32(originalIdPrescription);
+
+            return prescriptionDal.updateCorePrescription(currentPrescriptionDatarow);
+        }
+
+        public int insertPrescription(
+            string idPatient,
+            string conclusionMedical,
+            string price)
+        {
+
+            PrescriptionDTO currentPrescriptionDatarow = new PrescriptionDTO();
+            currentPrescriptionDatarow.IdPatient = Convert.ToInt32(idPatient);
+            currentPrescriptionDatarow.ConclusionMedical = Convert.ToString(conclusionMedical);
+            currentPrescriptionDatarow.Price = Convert.ToDecimal(price);
+
+            return prescriptionDal.insertCorePrescription(currentPrescriptionDatarow);
+        }
+
+        #endregion Logical CRUD Methods
+    }
+}
